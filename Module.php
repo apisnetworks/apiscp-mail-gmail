2e24f0f6d6a1ae0b8901e02e7c4f168350ec14ee
Matt Saladna <msaladna@apisnetworks.com>
2019-07-30 00:28:10 -0400

    Signature update
    Signed-off-by: Matt Saladna <msaladna@apisnetworks.com>

--- Module.php
+++ Module.php
@@ -19,27 +19,28 @@
 		 * Get DNS records
 		 *
 		 * @param string $domain
+		 * @param string $subdomain
 		 * @return array
 		 */
-		public function get_records(string $domain): array
+		public function get_records(string $domain, string $subdomain = ''): array
 		{
 			$ttl = $this->dns_get_default('ttl');
 
 			return [
 				new \Opcenter\Dns\Record($domain,
-					['name' => '', 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '1 ASPMX.L.GOOGLE.COM.']),
+					['name' => $subdomain, 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '1 ASPMX.L.GOOGLE.COM.']),
 				new \Opcenter\Dns\Record($domain,
-					['name' => '', 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '5 ALT1.ASPMX.L.GOOGLE.COM.']),
+					['name' => $subdomain, 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '5 ALT1.ASPMX.L.GOOGLE.COM.']),
 				new \Opcenter\Dns\Record($domain,
-					['name' => '', 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '5 ALT2.ASPMX.L.GOOGLE.COM.']),
+					['name' => $subdomain, 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '5 ALT2.ASPMX.L.GOOGLE.COM.']),
 				new \Opcenter\Dns\Record($domain,
-					['name' => '', 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '10 ASPMX2.GOOGLEMAIL.COM.']),
+					['name' => $subdomain, 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '10 ASPMX2.GOOGLEMAIL.COM.']),
 				new \Opcenter\Dns\Record($domain,
-					['name' => '', 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '10 ASPMX3.GOOGLEMAIL.COM.']),
+					['name' => $subdomain, 'ttl' => $ttl, 'rr' => 'MX', 'parameter' => '10 ASPMX3.GOOGLEMAIL.COM.']),
 				new \Opcenter\Dns\Record($domain,
-					['name' => 'mail', 'ttl' => $ttl, 'rr' => 'CNAME', 'parameter' => 'ghs.google.com.']),
+					['name' => rtrim("mail.${subdomain}",'.'), 'ttl' => $ttl, 'rr' => 'CNAME', 'parameter' => 'ghs.google.com.']),
 				new \Opcenter\Dns\Record($domain,
-					['name'      => '',
+					['name'      => $subdomain,
 					 'ttl'       => $ttl,
 					 'rr'        => 'TXT',
 					 'parameter' => '"v=spf1 include:_spf.google.com a ~all"'
